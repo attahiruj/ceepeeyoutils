df80c566f10767cdf80c63546ad5e774fa933140
Attahiru Jibril <attahiruj@gmail.com>
2026-02-05 15:30:37 +0200

    feat(preload): implement context bridge for electron integration

--- src/core/main.ts
+++ src/core/main.ts
@@ -1,10 +1,14 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
-import { isDev } from "./util.js";
+import { isDev, getPreloadPath } from "./util.js";
 import { pollResource } from "./resourceManager.js";
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath(),
+    },
+  });
 
   if (isDev()) {
     mainWindow.loadURL("http://localhost:1738");
